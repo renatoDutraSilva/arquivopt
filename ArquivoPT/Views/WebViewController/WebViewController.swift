e9cffc7d40e96b491d9d68516e1dc0f235fb8646
Paulo Rocha <paulorocha99@hotmail.com>
2019-04-02 04:08:21 +0100

    Added the webViewController

---
+++ ArquivoPT/Views/WebViewController/WebViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WebViewController.swift
+//  ArquivoPT
+//
+//  Created by Paulo Rocha on 02/04/2019.
+//  Copyright © 2019 Renato Silva. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
